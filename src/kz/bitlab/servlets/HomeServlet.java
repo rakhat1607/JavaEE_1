900c3df35caefc791828ce9d5c6fc23b98315b13
Rakhat Seitkanov <rakhat1607@yandex.kz>
2022-07-07 16:18:57 +0600

    Initial commit

---
+++ src/kz/bitlab/servlets/HomeServlet.java
@@ -0,0 +1,51 @@
+package kz.bitlab.servlets;
+
+
+
+import javax.servlet.ServletException;
+
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+
+
+@WebServlet(value = "/home")
+
+public class HomeServlet extends HttpServlet {
+
+
+
+    @Override
+
+    protected void doPost(HttpServletRequest request,
+
+                          HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+
+
+    @Override
+
+    protected void doGet(HttpServletRequest request,
+
+                         HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.print("<h1>First BITLAB Java EE Application</h1>");
+
+    }
+
+}
